190a62453b42233890ced2305e4a0b2b33fa0fce
Brian Otieno <bryomajor@gmail.com>
2019-09-03 16:06:31 +0300

    Make the nav to pick specific styling on scroll.

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,16 @@
         var st = $(this).scrollTop();
         // Scroll down
         if (st > lastScrollTop) {
-            navbar.fadeOut();
+            navbar.fadeIn();
+            navbar.removeClass('navbar-dark bg-transparent').addClass('navbar-light bg-custom');
         } 
         // Scroll up but still lower than 200 (change that to whatever suits your need)
         else if(st < lastScrollTop && st > 200) {
-            navbar.fadeIn();
-            navbar.removeClass('navbar-light bg-transparent').addClass('navbar-dark bg-custom');
+            navbar.removeClass('navbar-dark bg-transparent').addClass('navbar-light bg-custom');
         }
         // Reached top
         else {
-            navbar.removeClass('navbar-dark bg-custom').addClass('navbar-light bg-transparent');
+            navbar.removeClass('navbar-light bg-custom').addClass('navbar-dark bg-transparent');
         }
         lastScrollTop = st;
     });
